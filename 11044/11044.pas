05a70d05669599679182dd115f5a371d4b7942cb
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2017-09-05 03:01:02 +0200

    Create 11044.pas

---
+++ 11044/11044.pas
@@ -0,0 +1,12 @@
+program UVa11044(input, output);
+
+var t, n, m, i: integer;
+
+begin
+    readln(t);
+    for i := 1 to t do
+    begin
+        readln(n, m);
+        writeln((n div 3)*(m div 3));
+    end
+end.
