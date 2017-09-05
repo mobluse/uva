64c753e62e7ed4396efed1cda143ffc1dcf09310
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2017-09-05 02:59:29 +0200

    Create 11044.cpp

---
+++ 11044/11044.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; ++i) {
+        int n, m;
+        scanf("%d %d", &n, &m);
+        printf("%d\n", (n/3)*(m/3));
+    }
+}
