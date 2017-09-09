11bf91eb480133a3e3d50dcb12957cc903817d0d
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2017-09-09 19:01:31 +0200

    Update main.cpp

--- 11044/main.cpp
+++ 11044/main.cpp
@@ -1,6 +1,6 @@
 // @JUDGE_ID: mobluse 11044 C++ "Closed-form expression"
 // g++ -lm -lcrypt -O2 -pipe -DONLINE_JUDGE main.cpp
-// g++ -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE main.cpp
+// g++ -lm -lcrypt -O2 -pipe -std=c++11 -DONLINE_JUDGE main.cpp
 #include <stdio.h>
 
 int main() {
@@ -11,4 +11,5 @@ int main() {
         scanf("%d %d", &n, &m);
         printf("%d\n", (n/3)*(m/3));
     }
+    return 0;
 }
